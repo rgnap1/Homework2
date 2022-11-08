d2b3ae1ed1b0924a191cc88e274837b65eb5a8c0
rgnap <osovskaya_1993@mail.ru>
2022-11-08 21:15:21 +0300

    Commit 3 in Homework2. Finished task 2.

--- src/Main.java
+++ src/Main.java
@@ -37,6 +37,34 @@ public class Main {
         frog = frog+4;
         System.out.println (frog);
 
+        //task3
+
+        var theWeightABoxerOne = 78.2;
+        var theWeightABoxerTwo = 82.7;
+        var theTotalWeightOfBoxers = theWeightABoxerOne+theWeightABoxerTwo;
+        System.out.println ("Общий вес двух боксеров " + theTotalWeightOfBoxers + " кг! ");
+        var theBoxersWeightDifference = theWeightABoxerOne -  theWeightABoxerTwo;
+        System.out.println (" Разница между весом боксеров " + theBoxersWeightDifference + " кг! ");
+        var boxersWeightDifference  = theWeightABoxerTwo- theWeightABoxerOne;
+        System.out.println ("Вес второго боксера больше на " + boxersWeightDifference + " кг! ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
