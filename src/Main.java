4ece030f2e1407a29a2939cfe42e61872de6b00b
rgnap <osovskaya_1993@mail.ru>
2022-11-08 17:58:56 +0300

    Commit 2 in Homework2. Finished task 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        var dog = 8.0;
+        dog = dog+4;
+        dog = dog-3.5;
+        System.out.println (dog);
+
+        var cat = 3.6;
+        cat = cat+4;
+        cat = cat-1.6;
+        System.out.println (cat);
+
+        var paper = 763789;
+        paper = paper+4;
+        paper = paper-7639;
+        System.out.println (paper);
+
     }
 }
